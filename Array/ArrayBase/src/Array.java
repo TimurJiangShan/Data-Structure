d9a1fd5303343a0ded7ed6db7ad2db15bec6bc53
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-06-17 21:05:18 +1000

    Array Own

--- Array/ArrayBase/src/Array.java
+++ Array/ArrayBase/src/Array.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class Array {
+
+    private int[] data;
+    private int size;
+
+    /**
+     * @param capacity
+     * */
+    public Array(int capacity) {
+        data = new int[capacity];
+    }
+
+    // No para
+    public Array(){
+        this(10);
+    }
+
+    /**
+     * Get current number of elements
+     * */
+    public int getSize(){
+        return size;
+    }
+
+    /**
+     * Get Maximum number of array
+     * */
+    public int getCapacity(){
+        return data.length;
+    }
+
+    /**
+     * Null Check
+     * */
+    public boolean isEmpty(){
+        return size == 0;
+    }
 }
