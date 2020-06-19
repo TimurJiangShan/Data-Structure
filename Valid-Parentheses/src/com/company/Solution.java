0037ce1f6eed52863d101f2fadcff1e97422a7cf
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-06-19 15:54:26 +1000

    Stack basic

--- Valid-Parentheses/src/com/company/Solution.java
+++ Valid-Parentheses/src/com/company/Solution.java
@@ -23,10 +23,14 @@ public class Solution {
                     return false;
             }
         }
+
         if (stack.isEmpty())
             return true;
         else
             return false;
     }
 
+    public static void main(String[] args){
+        System.out.println((new Solution()).isValid("{{{{{}}}}}"));
+    }
 }
