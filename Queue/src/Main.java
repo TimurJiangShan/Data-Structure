e9c260343aac1cefbe39c3ca5d1b3980ee9cf7d7
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-06-20 17:58:46 +1000

    Stack basic

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -1,2 +1,35 @@
+import java.util.Random;
+
 public class Main {
+
+    private static double testQueue(Queue<Integer> q, int opCount){
+
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++) {
+            q.enqueue(random.nextInt(Integer.MAX_VALUE));
+        }
+
+        for (int i = 0; i < opCount; i++){
+            q.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args){
+
+        int opCount = 10000000;
+
+        // 主要是出队耗时
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = testQueue(arrayQueue, opCount);
+        System.out.println("ArrayQueue, time: " + time1 + " s");
+
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = testQueue(loopQueue, opCount);
+        System.out.println("LoopQueue, time: " + time2 + " s");
+    }
 }
