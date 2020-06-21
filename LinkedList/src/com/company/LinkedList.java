63aa9e5954e41cc558df520d7d10b1715b94f7bc
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-06-21 21:09:08 +1000

    Stack basic

--- LinkedList/src/com/company/LinkedList.java
+++ LinkedList/src/com/company/LinkedList.java
@@ -50,8 +50,61 @@ public class LinkedList<E> {
         node.next = head;
         head = node;
 
-        // head = new Node(e, head);
+        // head = new Node(e, head);  They do the same thing.
         size++;
+    }
+
+    // 在链表的index位置添加新节点
+
+    public void add(int index, E e){
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Illegal index");
+        }
+
+        if (index == 0) {
+            addFirst(e);
+        } else {
+            Node prev = head;
+
+            // 找到index的前一个元素 也就是index-1, 由于是用过prev = prev.next 来进行移动，所以终点是 index - 1,
+            // 也就是 prev.next = index - 1; 那么当前的i = index - 2;
+            for (int i = 0; i < index - 1; i++){
+                prev = prev.next;
+            }
+
+            prev.next = new Node(e, prev.next);
+            size++;
+        }
+    }
+
+    /*
+    * public void add(int index, E e){
+    *   if(index < 0 || index > size){
+    *       throw new Ill();
+    *   }
+    *
+    *   if(index = 0) {
+    *       addFirst(e);
+    *   } else {
+    *       Node prev = head;
+    *
+    *       for(int i = 0; i < index - 1; i ++) {
+    *           prev = prev.next;
+    *       }
+    *   }
+    *
+    *   Node node = new Node(e);
+    *   node = prev.next;
+    *   prev.next = node;
+    *   size++;
+    *
+    * }
+    *
+    *
+    * */
+
+    public void addLast(E e){
+        add(size, e);
     }
 
 
