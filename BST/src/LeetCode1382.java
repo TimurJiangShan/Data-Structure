a85457ae11336d94f315f23a08e406fdf2747768
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-02-17 18:23:28 +1100

    feat: add leetcode1382

---
+++ BST/src/LeetCode1382.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class LeetCode1382 {
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    private ArrayList<Integer> numbers;
+
+    public TreeNode balanceBST(TreeNode root) {
+        this.numbers = new ArrayList<>();
+        getInOrderArray(root);
+        return helper(0, numbers.size()-1);
+    }
+
+    private TreeNode helper(int left, int right){
+        if(left > right) return null;
+        int p = (left + right) / 2;
+        TreeNode root = new TreeNode(numbers.get(p));
+        root.left = helper(left, p-1);
+        root.right = helper(p+1, right);
+        return root;
+    }
+
+    private void getInOrderArray(TreeNode root){
+        if(root == null) return;
+        getInOrderArray(root.left);
+        this.numbers.add(root.val);
+        getInOrderArray(root.right);
+    }
+}
--- BST/src/Main.java
+++ BST/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -6,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         BST<Integer> bst = new BST<>();
-        int[] nums = {5, 3, 6, 8, 4, 2};
-        for(int num: nums)
-            bst.add(num);
+//        int[] nums = {5, 3, 6, 8, 4, 2};
+//        for(int num: nums)
+//            bst.add(num);
 
         /////////////////
         //      5      //
@@ -23,5 +24,11 @@ public class Main {
         System.out.println(bst);
 
         Queue<Integer> q = new LinkedList<>();
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+
     }
 }
