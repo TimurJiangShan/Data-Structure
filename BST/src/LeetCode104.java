84de7377b3a42094812eeeaed2b7894a3f7c89df
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-02-20 13:37:47 +1100

    feat: leetcode104

---
+++ BST/src/LeetCode104.java
@@ -0,0 +1,36 @@
+public class LeetCode104 {
+
+    /*
+    *
+    * 标签：DFS
+    找出终止条件：当前节点为空
+    找出返回值：节点为空时说明高度为 0，所以返回 0；节点不为空时则分别求左右子树的高度的最大值，同时加1表示当前节点的高度，返回该数值
+    某层的执行过程：在返回值部分基本已经描述清楚
+    时间复杂度：O(n)O(n)
+
+    *
+    *
+    * */
+      public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+}
