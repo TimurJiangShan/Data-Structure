544aa8d2910fb9e987f254458f3f0a58583d12d1
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-02-22 21:00:45 +1100

    feat: add leetcode1290 getDecimalValue

--- LinkedList/src/com/company/LeetCode1290.java
+++ LinkedList/src/com/company/LeetCode1290.java
@@ -1,2 +1,16 @@
-package com.company;public class LeetCode1290 {
+package com.company;
+
+public class LeetCode1290 {
+    public int getDecimalValue(ReverseBetween.ListNode head) {
+
+        int result = 0;
+        while(head != null) {
+            // 左移一位就是乘2，后移一位就是除2。 最后别忘了 result += result * 2;
+            result <<= 1;
+
+            result += result *2;
+            head = head.next;
+        }
+        return result;
+    }
 }
