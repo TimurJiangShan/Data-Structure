3026f774a3e445966e87428ed292dcede8baa221
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-02-28 17:40:04 +1100

    chore: add comment to leetcode938

--- BST/src/LeetCode938.java
+++ BST/src/LeetCode938.java
@@ -12,13 +12,21 @@ public class LeetCode938 {
       }
   }
 
+  /*
+  *
+  * 递归，如果当前节点在范围内，加到结果里。如果当前节点大于low，遍历左子树，小于high，遍历右子树
+  *
+  *
+  * */
+
     public int rangeSumBST(TreeNode root, int low, int high) {
         if(root == null) return 0;
 
         int result = 0;
         result += filter(root, root.val, low, high);
-        result += rangeSumBST(root.left, low, high);
-        result += rangeSumBST(root.right, low, high);
+
+        if(root.val > low)  result += rangeSumBST(root.left, low, high);
+        if(root.val < high) result += rangeSumBST(root.right, low, high);
 
         return result;
     }
