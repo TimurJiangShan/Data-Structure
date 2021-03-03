9329830a4633a679406c370302f18fc2ff337ad1
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-03-03 14:58:10 +1100

    feat: add leetcode110 balanced binary tree

---
+++ BST/src/LeetCode110.java
@@ -0,0 +1,26 @@
+public class LeetCode110 {
+      public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    public boolean isBalanced(TreeNode root) {
+        // 任意一个节点的左右子树的高度不会相差1
+        if(root == null) return true;
+        return Math.abs(getHeight(root.left) - getHeight(root.right)) < 2 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode root) {
+        if(root == null) return -1;
+        return 1 + Math.max(getHeight(root.left) , getHeight(root.right));
+
+    }
+}
