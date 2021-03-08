6b429d74aaeac44af593aa89c88c5b439357fb69
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-03-08 20:46:50 +1100

    chore: review binary tree maximum path sum

--- LintCode100Problem/src/BinaryTreeMaximumPathSum.java
+++ LintCode100Problem/src/BinaryTreeMaximumPathSum.java
@@ -14,16 +14,14 @@ public class BinaryTreeMaximumPathSum {
         * maxPath: 从树中任意到任意点的最大路径，这条路径至少包含一个点
         * */
         int singlePath, maxPath;
-        ResultType(int singlePath, int maxPath){
+        public ResultType(int singlePath, int maxPath){
             this.singlePath = singlePath;
             this.maxPath = maxPath;
         }
     }
 
     private ResultType helper(TreeNode root){
-        if (root == null) {
-            return new ResultType(0, Integer.MIN_VALUE);
-        }
+        if(root == null) return new ResultType(0, Integer.MIN_VALUE);
 
         // Divide
         ResultType left = helper(root.left);
@@ -31,14 +29,13 @@ public class BinaryTreeMaximumPathSum {
 
         // Conquer
         int singlePath = Math.max(left.singlePath, right.singlePath) + root.val;
-
-        // 为啥这里要跟0比较大小啊
         singlePath = Math.max(singlePath, 0);
 
         int maxPath = Math.max(left.maxPath, right.maxPath);
-        maxPath = Math.max(maxPath, left.singlePath + right.singlePath + root.val);
+        maxPath = Math.max(left.singlePath + right.singlePath + root.val, maxPath);
 
         return new ResultType(singlePath, maxPath);
+
     }
 
     public int maxPathSum(TreeNode root) {
