b2186d7fa31aaa63531b0c19fae628e4bc692bef
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-03-11 22:18:17 +1100

    chore: add comment

--- LintCode100Problem/src/StrStr.java
+++ LintCode100Problem/src/StrStr.java
@@ -4,7 +4,7 @@ public class StrStr {
             return -1;
 
         for (int i = 0; i < source.length() - target.length() + 1;i++) {
-            int j = 0;
+            int j = 0; // 在这里定义j，后面的for循环里面的j会覆盖这个j吗？？
             for (j = 0; j < target.length(); j++) {
                 if (source.charAt(i+j) != target.charAt(j)) {
                     break;
