6f1769a660f3b02c19c394c49dab6e695a12a51f
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-03-13 22:23:05 +1100

    feat: add longest palindromic substring

---
+++ LintCode100Problem/src/LongestPalindromicSubstring.java
@@ -0,0 +1,34 @@
+public class LongestPalindromicSubstring {
+    // 动态规划解法
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() == 0) return "";
+
+        int n = s.length();
+        boolean[][] isPalidrome = new boolean[n][n];
+
+        int longest = 1, start = 0;
+        for (int i = 0; i < n; i++) {
+            isPalidrome[i][i] = true;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            isPalidrome[i][i+1] = s.charAt(i) == s.charAt(i+1);
+            if(isPalidrome[i][i+1]){
+                start = i;
+                longest = 2;
+            }
+        }
+
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = i+2; j < n; j++){
+                isPalidrome[i][j] = isPalidrome[i+1][j+1] && s.charAt(i) == s.charAt(j);
+                if (isPalidrome[i][j] && j-i+1 > longest) {
+                    start = i;
+                    longest = j-i+1;
+                }
+            }
+        }
+
+        return s.substring(start, start + longest);
+    }
+}
