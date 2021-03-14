d6433ceea685aec7883a3e3b4aaa8f1b5953ba37
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-03-14 21:48:40 +1100

    bugix: fix longest palindrome

--- LintCode100Problem/src/LongestPalindromicSubstring.java
+++ LintCode100Problem/src/LongestPalindromicSubstring.java
@@ -1,5 +1,9 @@
 public class LongestPalindromicSubstring {
     // 动态规划解法
+    /*
+    * Build a 2D Boolean matrix with size N*N, element[i,j] is
+    * True if sub-string S[i]...S[j] is a palindromic, otherwise False;
+    * */
     public String longestPalindrome(String s) {
         if(s == null || s.length() == 0) return "";
 
@@ -21,7 +25,7 @@ public class LongestPalindromicSubstring {
 
         for (int i = n-1; i >= 0; i--) {
             for (int j = i+2; j < n; j++){
-                isPalidrome[i][j] = isPalidrome[i+1][j+1] && s.charAt(i) == s.charAt(j);
+                isPalidrome[i][j] = isPalidrome[i+1][j-1] && s.charAt(i) == s.charAt(j);
                 if (isPalidrome[i][j] && j-i+1 > longest) {
                     start = i;
                     longest = j-i+1;
