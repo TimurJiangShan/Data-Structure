ce2d1894e59ddeb62634ab5d0ec42bd3196fb640
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-03-17 21:47:37 +1100

    feat: add Kth largest element version2

--- Sort/src/KthLargestElement.java
+++ Sort/src/KthLargestElement.java
@@ -1,41 +1,45 @@
 public class KthLargestElement {
-    public int kthLargestElement(int k, int[] nums){
-        if (nums == null || nums.length == 0 || k < 1 || k > nums.length){
+    // Version1： 从大到小排序， 然后取第N个元素
+    public int kthLargestElement(int n, int[] nums) {
+        // write your code here
+        if (nums == null || nums.length == 0 || n < 1 || n > nums.length){
             return -1;
         }
-        // 第K大，意味着总后往前是 长度 - K
-        return partition(nums, 0, nums.length-1, nums.length-k);
+
+        return partition(nums, 0, nums.length-1, n);
     }
 
     private int partition(int[] nums, int start, int end, int k){
         if (start >= end) {
-            return nums[k];
+            return nums[start];
         }
 
         int left = start, right = end;
-        int pivot = nums[(start + end)/2];
+        int pivot = nums[(left + right)/2];
         while (left <= right) {
-            while (left <= right && nums[left] < pivot) {
+            while (left <= right && nums[left] > pivot) {
                 left++;
             }
 
-            while (left <= right && nums[right] > pivot) {
-                right++;
+            while (left <= right && nums[right] < pivot) {
+                right--;
             }
 
             if (left <= right) {
                 swap(nums, left, right);
+                left++;
+                right--;
             }
         }
 
-        if (k <= right) {
+        if (start + k - 1 <= right) {
             return partition(nums, start, right, k);
         }
-        if (k >= left) {
-            return partition(nums, left, end, k);
+        if (start + k - 1 >= left) {
+            return partition(nums, left, end, k - (left - start));
         }
 
-        return nums[k];
+        return nums[right+1];
     }
 
     private void swap(int[] nums, int i, int j){
@@ -43,4 +47,54 @@ public class KthLargestElement {
         nums[j] = nums[i];
         nums[i] = temp;
     }
+
+    // Version2：从小到大排序，取第 （数组长度 - N）大的元素
+    /*
+    *  public int kthLargestElement(int n, int[] nums) {
+        // write your code here
+        if(nums == null || nums.length == 0 || n < 1 || n > nums.length){
+            return -1;
+        }
+        return partition(nums, 0, nums.length-1, nums.length - n);
+    }
+
+    private int partition(int[] nums, int start, int end, int k){
+        if(start >= end) {
+            return nums[k];
+        }
+        int left = start, right = end;
+        int pivot = nums[(left+right)/2];
+
+        while(left <= right){
+            while(left <= right && nums[left] < pivot){
+                left++;
+            }
+            while(left <= right && nums[right] > pivot){
+                right--;
+            }
+
+            if(left <= right) {
+                swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        if(k <= right) {
+            return partition(nums, start, right, k);
+        }
+        if(k >= left) {
+            return partition(nums, left, end, k);
+        }
+
+        return nums[k];
+    }
+
+    private void swap(int[] num, int i, int j){
+        int temp = num[j];
+        num[j] = num[i];
+        num[i] = temp;
+    }
+    *
+    * */
 }
