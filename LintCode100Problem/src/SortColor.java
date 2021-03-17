225057a4fe650c82c4748b550fa64e6572aeaf11
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-03-17 23:12:33 +1100

    chore: change readme

--- LintCode100Problem/src/SortColor.java
+++ LintCode100Problem/src/SortColor.java
@@ -38,4 +38,16 @@ public class SortColor {
         nums[j] = nums[i];
         nums[i] = temp;
     }
+
+    // Version2 : 采用双指针的方法，一遍扫描即可。时间复杂度为O(n), 空间复杂度是常量空间(Constant)
+
+    /*
+    *
+        我们建立首尾双指针left和right，分别指示0/1边界和1/2边界。left左边（不含left）全为0，right右边（不含right）全为2。初始化left和mid为0，right 为len(nums)-1。
+        第三个指针mid从left起向right移动，边扫描边实时更新两个边界。
+        若 nums[mid]为 0 ：交换第 mid个和第left个元素，并将 left 指针和mid指针都向右移。
+        若 nums[mid]为 2 ：交换第 mid个和第 right个元素，并将 right指针左移
+        若 nums[mid]为 1 ：将指针mid右移。
+        补充一下，当mid与left交换后，mid能够后移，因为此时nums[mid]可能为0，后面还需要与left交换。
+    * */
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Data-Structure
-**LeetCode Practices and fucking Algorithm!!!**
+**LeetCode**
+* 常用数据结构以及复杂度
+  
+   O(1)	位运算	常数级复杂度，一般面试中不会有 <br/>
+   O(logn)	二分法，倍增法，快速幂算法，辗转相除法 <br/>
+   O(n)	枚举法，双指针算法，单调栈算法，KMP算法，Rabin Karp，Manacher's Algorithm	又称作线性时间复杂度 <br/>
+   O(nlogn)	快速排序，归并排序，堆排序 <br/>
+   O(n^2)	枚举法，动态规划，Dijkstra <br/>
+   O(n^3)	枚举法，动态规划，Floyd <br/>
+   O(2^n)	与组合有关的搜索问题 <br/>
+   O(n!)	与排列有关的搜索问题 <br/>
