8f80964c02de57862f9be6f9fa0640ed5f5aca22
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-03-27 01:08:08 +1100

    chore: add packages and remove unused code

--- LintCode100Problem/src/TwoSumDataStructure.java
+++ LintCode100Problem/src/TwoSumDataStructure.java
@@ -1,4 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TwoSum {
+
+	/*
+	* // Version2
+    public int twoSum6(int[] nums, int target) {
+     if ( nums == null || nums.length < 2 ) {
+        return 0;
+    }
+
+	int count = 0;
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for ( int i = 0; i < nums.length; i++ ) {
+        map.put( nums[i], map.getOrDefault(nums[i], 0) +  1);
+    }
+
+    for ( int key : map.keySet() ) {
+        int diff = target - key;
+        if ( (diff == key && map.get(diff) > 1 )
+            || ( diff != key && map.containsKey(diff) && key < diff)) {
+            count++;
+        }
+    }
+    return count;
+
+    }
+	*
+	* */
 	private List<Integer> list = null;
 	private Map<Integer, Integer> map = null;
 	public TwoSum(){
@@ -27,29 +59,7 @@ public class TwoSum {
 	}
 }
 
-// Version2
-    public int twoSum6(int[] nums, int target) {
-     if ( nums == null || nums.length < 2 ) {
-        return 0;
-    }
-    
-	int count = 0;
-    Map<Integer, Integer> map = new HashMap<>();
-    
-    for ( int i = 0; i < nums.length; i++ ) {
-        map.put( nums[i], map.getOrDefault(nums[i], 0) +  1);
-    }
-        
-    for ( int key : map.keySet() ) {
-        int diff = target - key;
-        if ( (diff == key && map.get(diff) > 1 ) 
-            || ( diff != key && map.containsKey(diff) && key < diff)) {
-            count++;
-        }
-    }
-    return count;
-    
-    }
+
 
 
 
