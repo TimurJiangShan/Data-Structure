c55de07b0aff1420ad5120067654a1a572698bd5
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-04-07 21:17:10 +1000

    feat: add circumference

---
+++ LintCode100Problem/src/CircumferenceAndArea.java
@@ -0,0 +1,8 @@
+public class CircumferenceAndArea {
+    public double[] calculate(int r) {
+        double[] a = new double[2];
+        a[0] = 2 * 3.14 * r;
+        a[1] = 3.14 * r * r;
+        return a;
+    }
+}
