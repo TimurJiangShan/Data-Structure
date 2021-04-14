a7720a244848dc406d823afc058097e81e5af24e
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-04-14 21:30:46 +1000

    feat: add anagram

---
+++ LintCode100Problem/src/Anagrams.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Anagrams {
+    public List<String> anagrams(String[] strs) {
+        List<String> result = new ArrayList<String>();
+        if (strs == null || strs.length == 0) {
+            return result;
+        }
+        Map<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] arr = strs[i].toCharArray();
+            Arrays.sort(arr);
+            String s = String.valueOf(arr);
+            if (!map.containsKey(s)) {
+                ArrayList<String> list = new ArrayList<String>();
+                map.put(s, list);
+            }
+            map.get(s).add(strs[i]);
+        }
+        for (Map.Entry<String, ArrayList<String>> entry : map.entrySet()) {
+            if (entry.getValue().size() >= 2) {
+                result.addAll(entry.getValue());
+            }
+        }
+        return result;
+    }
+}
+}
