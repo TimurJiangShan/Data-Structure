a7c93d3d47b2de7d02698c2a23c8ffe466f2a08a
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-05-21 23:47:59 +1000

    feat: add interest subarray

---
+++ LintCode100Problem/src/InterestingSubarray.java
@@ -0,0 +1,21 @@
+public class Solution {
+  public int maxLen(int[] tree) {
+    int cur = 0, max = 0, a = 0, b = 0, count_b = 0;
+    for (int num : tree) {
+      if (num == a || num == b) {
+        cur++;
+      } else {
+        cur = count_b + 1;
+      }
+      if (num == b) {
+        count_b++;
+      } else {
+        count_b = 1;
+        a = b;
+        b = num;
+      }
+      max = Math.max(max, cur);
+    }
+    return max;
+  }
+}
