742412f4f3a6a77e91e0b4b8417d2c2b2a2294e7
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-06-13 20:50:00 +1000

    feat: add min difference

---
+++ LintCode100Problem/src/MinimumDifferenceBetweenBSTNodes.java
@@ -0,0 +1,18 @@
+public class MinimumDifferenceBetweenBSTNodes {
+  Integer res = Integer.MAX_VALUE, pre = null;
+
+  public int minDiffInBST(TreeNode root) {
+    // Write your code here
+    if (root.left != null) {
+      minDiffInBST(root.left);
+    }
+    if (pre != null) {
+      res = Math.min(res, root.val - pre);
+    }
+    pre = root.val;
+    if (root.right != null) {
+      minDiffInBST(root.right);
+    }
+    return res;
+  }
+}
